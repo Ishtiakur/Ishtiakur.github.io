0c68095d1f16678aef6f4ae6fc27ebaf5d22f0e4
Ishtiak <ishtiak24014@gmail.com>
2018-01-11 19:55:40 +0600

    update
    update

--- htmltemplate/js/custom.js
+++ htmltemplate/js/custom.js
@@ -11,9 +11,12 @@ $(document).ready(function() {
   });
 });
 
+
+
 /*==================
        Team
 ==================*/
+
 $(function(){
   $("#team-members").owlCarousel({
   
@@ -33,13 +36,100 @@ $(function(){
 $(function(){
  $("#customers-testimonials").owlCarousel({
  
-   items:1,
-     autoplay:true;
+     items:1,
+     autoplay:true,
      smartSpeed:700,
      loop:true,
      autoplayHoverPause:true,
  
- })
+ });
 
-})
+});
+
+/*==================
+       STATS
+==================*/
+$(function(){
+    
+     $('.counter').counterUp({
+                delay: 10,
+                time: 3000
+            });
+      })
+/*==================
+       CLIENT
+==================*/
+
+$(function(){
+  $("#clients-list").owlCarousel({
+  
+      items: 6,
+      autoplay: true,
+      smartSpeed:700,
+      loop:true,
+      autoplayHoverPause:true
+  
+  });
+});
+
+/*==================
+      NAVIGATIO 
+==================*/
+$(function(){
+ $(window).scroll(function(){
+ 
+     if($(this).scrollTop()<60){
+        
+        $("nav").removeClass("vesco-top-nav");
+     
+        }
+    else{
+        $("nav").addClass("vesco-top-nav");
+        
+        }
+        
+ });
+
+
+});
+
+/*==================
+    SMOTH LIST
+==================*/
+$(function(){
+$("a.smooth-scroll").click(function(Event){
+
+Event.preventDefault();
+    var section =$(this).attr("href");
+    $('html,body').animate({
+     scrollTop:$(section).offset().top -64
+    },1250);
+});
+
+});
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
